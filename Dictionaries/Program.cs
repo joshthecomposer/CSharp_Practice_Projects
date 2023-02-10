e4719b0a406ff17c9e54cb95ce3b786f2738c621
Joshua Wise <joshuawise@joshuas-mbp.myfiosgateway.com>
2023-02-10 15:29:50 -0500

    Dictionaries

---
+++ Dictionaries/Program.cs
@@ -0,0 +1,20 @@
+﻿Dictionary<string, string> profile = new Dictionary<string, string>();
+
+
+profile.Add("Name", "Daniel");
+profile.Add("Email","d@gmail.com");
+profile.Add("Location","NYC");
+
+
+Console.WriteLine("Student profile:");
+
+foreach(string key in profile.Keys)
+{
+    Console.WriteLine(key + ": " + profile[key]);
+}
+
+foreach(KeyValuePair<string, string> entry in profile)
+{
+    Console.WriteLine(entry.Key + ": " + entry.Value);
+}
+
