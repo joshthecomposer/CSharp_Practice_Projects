5859983a52616b2c7406ed3c6db2e94684b7e61e
Joshua Wise <joshuawise@joshuas-mbp.myfiosgateway.com>
2023-02-10 14:54:51 -0500

    Did FizzBuzz with CSharp

--- Fundamentals_I/Program.cs
+++ Fundamentals_I/Program.cs
@@ -1,2 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Random rd = new Random();
+
+static void d() {
+    Console.WriteLine("===============================");
+}
+
+for (int i = 1; i < 256; i++)
+{
+    Console.WriteLine(i);
+}
+
+d();
+
+for (int i = 0; i < 5; i++)
+{
+    int n = rd.Next(10, 21);
+    Console.WriteLine(n);
+}
+
+d();
+
+int sum = 0;
+for (int i = 0; i < 5; i++)
+{
+    sum += rd.Next(10,21);
+}
+
+Console.WriteLine(sum);
+
+d();
+
+void fizzBuzz() {
+    for (int i = 1; i < 101; i++)
+    {
+        if (i % 3 == 0 && i % 5 == 0) {
+            Console.WriteLine("FizzBuzz");
+        } 
+        else if (i % 3 == 0) 
+        {
+            Console.WriteLine("Fizz");
+        } 
+        else if (i % 5 == 0) 
+        {
+            Console.WriteLine("Buzz");
+        }
+        else 
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
+
+fizzBuzz();
+d();
