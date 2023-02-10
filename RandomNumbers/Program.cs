55ec01e66b63d682b5543a47183a3ad7a534ac00
Joshua Wise <joshuawise@joshuas-mbp.myfiosgateway.com>
2023-02-10 14:37:37 -0500

    init

---
+++ RandomNumbers/Program.cs
@@ -0,0 +1,13 @@
+﻿Random rd = new Random();
+
+for (int i = 0; i < 11; i++) 
+{
+    int n = rd.Next(1, 101);
+    Console.WriteLine(n);
+}
+
+for (int i = 0; i < 11; i++)
+{
+    double d = rd.NextDouble();
+    Console.WriteLine(d);
+}
---
+++ VariablesAndTypes/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+int number = 32;
+bool success = true;
+double height = 1.2;
+
+Console.WriteLine($"The number is {number} and the height is {height} and the bool is {success}");
+
+Console.WriteLine("The number is " + number + " and the height is " + height + " and the bool is " + success);
+
+Console.WriteLine("{0} {1} {2}", number, height, success);
+
+for(int i = 0; i < number; i++){
+    Console.WriteLine(i);
+}
+
