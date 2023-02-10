21937e14d363a9fa25ec858283b5ada39c4c5863
Joshua Wise <joshuawise@joshuas-mbp.myfiosgateway.com>
2023-02-10 15:19:38 -0500

    Lists

---
+++ Lists/Program.cs
@@ -0,0 +1,29 @@
+﻿List<string> bikes = new List<string>();
+
+bikes.Add("Kawasaki");
+bikes.Add("Triumph");
+bikes.Add("BMW");
+bikes.Add("Moto Guzzi");
+bikes.Add("Harley Davidson");
+bikes.Add("Suzuki");
+
+// for (int idx = 0; idx < bikes.Count; idx++)
+// {
+//     Console.WriteLine("_" + bikes[idx]);
+// }
+
+bikes.Insert(2, "ASSSSS");
+
+// for (int idx = 0; idx < bikes.Count; idx++)
+// {
+//     Console.WriteLine("_" + bikes[idx]);
+// }
+
+bool removed = bikes.Remove("BMW");
+
+foreach(string m in bikes)
+{
+    Console.WriteLine("_" + m);
+}
+
+Console.WriteLine(removed);
