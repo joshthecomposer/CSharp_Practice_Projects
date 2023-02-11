9f41d8e89629b334d96c6deb4f2883e6093892d7
Joshua Wise <joshuawise@joshuas-mbp.myfiosgateway.com>
2023-02-11 10:23:01 -0500

    RideMaker Complete

---
+++ RideMaker/Program.cs
@@ -0,0 +1,14 @@
+﻿Vehicle geoPrism = new Vehicle("Geo Prism", 3, "Gray", true);
+Vehicle jeepWrangler = new Vehicle("Jeep Wrangler", "Black");
+Vehicle toyotaCamry = new Vehicle("Toyota Camry", "Blue");
+Vehicle nissanUltima = new Vehicle("Nissan Ultima", "Red");
+
+List<Vehicle> vehicles = new List<Vehicle>{geoPrism, jeepWrangler, toyotaCamry, nissanUltima};
+
+foreach (Vehicle vehicle in vehicles)
+{
+    vehicle.ShowInfo();
+}
+
+toyotaCamry.Travel(100);
+toyotaCamry.ShowInfo();//This is essentially a big ol' getter method
---
+++ RideMaker/Vehicle.cs
@@ -0,0 +1,40 @@
+class Vehicle
+{
+    string Name;
+    int NumPassengers;
+    string Color;
+    bool HasEngine = true;
+    int Mileage = 0;
+
+    public Vehicle(string name, int numPassengers,string color,bool hasEngine)
+    {
+        this.Name = name;
+        this.NumPassengers = numPassengers;
+        this.Color = color;
+        this.HasEngine = hasEngine;
+    }
+
+    public Vehicle(string name, string color)
+    {
+        this.Name = name;
+        this.Color = color;
+    }
+
+    public void ShowInfo()
+    {
+        Console.WriteLine("======================");
+        Console.WriteLine(this.Name + " Info:");
+        Console.WriteLine("Passengers : " + this.NumPassengers);
+        Console.WriteLine("Color : " + this.Color);
+        Console.WriteLine("Engine??? : " + this.HasEngine);
+        Console.WriteLine("Mileage : " + this.Mileage);
+    }
+
+    public void Travel(int miles)
+    {
+        this.Mileage += miles;
+        Console.WriteLine("======================");
+        Console.WriteLine(this.Name+" traveled "+this.Mileage+" miles.");
+    }
+
+}
